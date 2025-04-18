e9d829ea2a62866912f34fa2c0eb85e6535db416
Artanidos <japp.olaf@gmail.com>
2025-04-18 18:06:38 +0200

    upd

--- app.sml
+++ app.sml
@@ -77,19 +77,59 @@ App {
         scrim: "#000000"
     }
 // deployment start - don't edit here
-Deployment {
-File { path: "dao.png" time: "2025.03.19 21.59.42" type: "image" }
-  File { path: "cover-en.png" time: "2025.03.22 12.10.49" type: "image" }
-  File { path: "magic_book.png" time: "2025.03.21 23.15.03" type: "image" }
-  File { path: "begood.png" time: "2025.03.22 15.31.34" type: "image" }
-  File { path: "kids.png" time: "2025.03.22 00.10.02" type: "image" }
-  File { path: "circle.png" time: "2025.03.22 00.19.23" type: "image" }
-  File { path: "dome.png" time: "2025.03.22 00.02.17" type: "image" }
-  File { path: "garden.png" time: "2025.03.22 00.15.02" type: "image" }
-  File { path: "guttun.png" time: "2025.03.22 15.28.52" type: "image" }
-  File { path: "kitchen.png" time: "2025.03.22 00.03.21" type: "image" }
-  File { path: "cover-de.png" time: "2025.03.21 23.50.44" type: "image" }
-  File { path: "rainbow.png" time: "2025.04.18 07.47.05" type: "image" }
-}
+    Deployment {
+        File { path: "about.sml" time: "2025.04.18 08.14.09" type: "page-en" }
+        File { path: "struktur.sml" time: "2025.04.18 07.43.12" type: "page-en" }
+        File { path: "erinnerung.sml" time: "2025.04.17 21.54.58" type: "page-en" }
+        File { path: "mitgestaltung.sml" time: "2025.04.18 08.12.34" type: "page-en" }
+        File { path: "gift.sml" time: "2025.04.18 07.42.14" type: "page-en" }
+        File { path: "kreis.sml" time: "2025.04.18 07.43.38" type: "page-en" }
+        File { path: "home.sml" time: "2025.04.18 07.47.16" type: "page-en" }
+        File { path: "manifest.sml" time: "2025.04.18 08.13.36" type: "page-en" }
+        File { path: "vision.sml" time: "2025.04.18 07.42.52" type: "page-en" }
+        File { path: "mannifest.sml" time: "2025.04.18 08.11.28" type: "page-en" }
+        File { path: "about.sml" time: "2025.04.18 08.56.51" type: "page-de" }
+        File { path: "struktur.sml" time: "2025.04.17 21.34.24" type: "page-de" }
+        File { path: "erinnerung.sml" time: "2025.04.18 08.56.58" type: "page-de" }
+        File { path: "mitgestaltung.sml" time: "2025.04.18 08.55.50" type: "page-de" }
+        File { path: "gift.sml" time: "2025.04.18 08.57.05" type: "page-de" }
+        File { path: "kreis.sml" time: "2025.04.18 08.56.04" type: "page-de" }
+        File { path: "home.sml" time: "2025.04.18 08.56.17" type: "page-de" }
+        File { path: "manifest.sml" time: "2025.04.18 08.55.58" type: "page-de" }
+        File { path: "vision.sml" time: "2025.04.17 21.32.36" type: "page-de" }
+        File { path: "gift.md" time: "2025.04.18 08.59.49" type: "part-en" }
+        File { path: "struktur.md" time: "2025.04.18 08.44.32" type: "part-en" }
+        File { path: "kreis.md" time: "2025.04.18 08.39.30" type: "part-en" }
+        File { path: "home.md" time: "2025.04.18 09.01.40" type: "part-en" }
+        File { path: "manifest.md" time: "2025.04.18 08.40.31" type: "part-en" }
+        File { path: "vision.md" time: "2025.04.18 09.27.57" type: "part-en" }
+        File { path: "mitgestaltung.md" time: "2025.04.18 08.43.11" type: "part-en" }
+        File { path: "manifest2.md" time: "2025.04.18 08.42.22" type: "part-en" }
+        File { path: "about.md" time: "2025.04.18 08.35.35" type: "part-en" }
+        File { path: "erinnerung.md" time: "2025.04.18 08.37.28" type: "part-en" }
+        File { path: "gift.md" time: "2025.04.18 08.03.29" type: "part-de" }
+        File { path: "struktur.md" time: "2025.04.18 08.30.10" type: "part-de" }
+        File { path: "kreis.md" time: "2025.04.18 08.25.38" type: "part-de" }
+        File { path: "home.md" time: "2025.04.18 08.24.44" type: "part-de" }
+        File { path: "manifest.md" time: "2025.04.18 08.26.52" type: "part-de" }
+        File { path: "vision.md" time: "2025.04.18 09.28.07" type: "part-de" }
+        File { path: "mitgestaltung.md" time: "2025.04.18 08.31.50" type: "part-de" }
+        File { path: "manifest2.md" time: "2025.04.18 15.40.20" type: "part-de" }
+        File { path: "about.md" time: "2025.04.18 08.24.15" type: "part-de" }
+        File { path: "erinnerung.md" time: "2025.04.18 08.23.22" type: "part-de" }
+        File { path: "dao.png" time: "2025.03.19 21.59.42" type: "image" }
+        File { path: "cover-en.png" time: "2025.03.22 12.10.49" type: "image" }
+        File { path: "magic_book.png" time: "2025.03.21 23.15.03" type: "image" }
+        File { path: "begood.png" time: "2025.03.22 15.31.34" type: "image" }
+        File { path: "kids.png" time: "2025.03.22 00.10.02" type: "image" }
+        File { path: "circle.png" time: "2025.03.22 00.19.23" type: "image" }
+        File { path: "dome.png" time: "2025.03.22 00.02.17" type: "image" }
+        File { path: "garden.png" time: "2025.03.22 00.15.02" type: "image" }
+        File { path: "guttun.png" time: "2025.03.22 15.28.52" type: "image" }
+        File { path: "kitchen.png" time: "2025.03.22 00.03.21" type: "image" }
+        File { path: "cover-de.png" time: "2025.03.21 23.50.44" type: "image" }
+        File { path: "rainbow.png" time: "2025.04.18 07.47.05" type: "image" }
+
+    }
 // deployment end
 }
--- upd_deploy.py
+++ upd_deploy.py
@@ -1,65 +1,50 @@
 #
-# Copyright (C) 2024 CrowdWare
+# Copyright (C) 2025 CrowdWare
 #
-# This file is part of NoCodeDesigner.
+# This file is part of FreeBookDesigner.
 #
-#  NoCodeDesigner is free software: you can redistribute it and/or modify
+#  FreeBookDesigner is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
 #
-#  NoCodeDesigner is distributed in the hope that it will be useful,
+#  FreeBookDesigner is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
 #
 #  You should have received a copy of the GNU General Public License
-#  along with NoCodeDesigner.  If not, see <http://www.gnu.org/licenses/>.
+#  along with FreeBookDesigner.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 import os
 import re
 from datetime import datetime
 
-"""Add file entries to the deployment descriptor."""
 
+"""add file to the deploment descriptor"""
 def update_deployment(app_sml_path, deployment_data):
     with open(app_sml_path, 'r') as file:
         app_sml_content = file.read()
 
-    # Check if the file ends with a closing brace and remove it temporarily
-    ends_with_brace = app_sml_content.rstrip().endswith('}')
-    if ends_with_brace:
-        app_sml_content = app_sml_content.rstrip()[:-1].rstrip()  # Remove the final brace
-
-    # Match the deployment section and ensure it starts and ends correctly
     deployment_section_regex = re.compile(r"// deployment start.*?// deployment end", re.DOTALL)
 
-    # Create the deployment block content
     deployment_block = f"""// deployment start - don't edit here
-Deployment {{
+    Deployment {{
 {deployment_data}
-}}
+    }}
 // deployment end"""
 
-    # Replace or add the deployment block
     if deployment_section_regex.search(app_sml_content):
-        # Replace existing deployment section
         app_sml_content = deployment_section_regex.sub(deployment_block, app_sml_content)
     else:
-        # Append if not found
-        app_sml_content = app_sml_content + '\n' + deployment_block
+        app_sml_content = app_sml_content.rstrip('}') + '\n' + deployment_block + '\n}'
 
-    # Re-add the final brace if it was there initially
-    if ends_with_brace:
-        app_sml_content += '\n}'
-
-    # Write back the modified content
     with open(app_sml_path, 'w') as file:
         file.write(app_sml_content)
 
 
-"""Generate a list of files and their last modified date for deployment data."""
+"""gets a list of files and there last change date"""
 def generate_deployment_data(type, base_path, exclude_files=None):
     if exclude_files is None:
         exclude_files = []
@@ -71,36 +56,55 @@ def generate_deployment_data(type, base_path, exclude_files=None):
                 file_path = os.path.relpath(os.path.join(dirpath, filename), base_path)
                 mod_time = os.path.getmtime(os.path.join(dirpath, filename))
                 formatted_time = datetime.utcfromtimestamp(mod_time).strftime('%Y.%m.%d %H.%M.%S')
-                # Append each entry in the correct format
-                deployment_entries.append(f'  File {{ path: "{file_path}" time: "{formatted_time}" type: "{type}" }}')
-
-    # Return formatted entries as a single string
-    return "\n".join(deployment_entries)
+                deployment_entries.append(f'        File {{ path: "{file_path}" time: "{formatted_time}" type: "{type}" }}')
+    if not deployment_entries:
+        return ""
+    return "\n".join(deployment_entries) + "\n"
 
 
 def update():
     base_path = os.getcwd()
     app_sml_path = os.path.join(base_path, 'app.sml')
 
-    # Paths for various types of deployment data
-    paths = {
-        "page": os.path.join(base_path, 'pages'),
-        "part": os.path.join(base_path, 'parts'),
-        "image": os.path.join(base_path, 'images'),
-        "sound": os.path.join(base_path, 'sounds'),
-        "video": os.path.join(base_path, 'videos'),
-        "texture": os.path.join(base_path, 'textures'),
-        "model": os.path.join(base_path, 'models'),
-    }
+    pages_en_path = os.path.join(base_path, 'pages-en')
+    pages_de_path = os.path.join(base_path, 'pages-de')
+    pages_es_path = os.path.join(base_path, 'pages-es')
+    pages_pt_path = os.path.join(base_path, 'pages-pt')
+    pages_fr_path = os.path.join(base_path, 'pages-fr')
+    pages_eo_path = os.path.join(base_path, 'pages-eo')
+    parts_en_path = os.path.join(base_path, 'parts-en')
+    parts_de_path = os.path.join(base_path, 'parts-de')
+    parts_es_path = os.path.join(base_path, 'parts-es')
+    parts_pt_path = os.path.join(base_path, 'parts-pt')
+    parts_fr_path = os.path.join(base_path, 'parts-fr')
+    parts_eo_path = os.path.join(base_path, 'parts-eo')
+    images_path = os.path.join(base_path, 'images')
+    sounds_path = os.path.join(base_path, 'sounds')
+    videos_path = os.path.join(base_path, 'videos')
+    textures_path = os.path.join(base_path, 'textures')
+    models_path = os.path.join(base_path, 'models')
 
-    # Collect deployment data for all paths and types
-    deployment_data = ""
-    for data_type, path in paths.items():
-        deployment_data += generate_deployment_data(data_type, path, exclude_files=['.DS_Store']) + "\n"
+    deployment_data = generate_deployment_data("page-en", pages_en_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("page-de", pages_de_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("page-es", pages_es_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("page-pt", pages_pt_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("page-fr", pages_fr_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("page-eo", pages_eo_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-en", parts_en_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-de", parts_de_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-es", parts_es_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-pt", parts_pt_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-fr", parts_fr_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("part-eo", parts_eo_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("image", images_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("sound", sounds_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("video", videos_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("texture", textures_path, exclude_files=['.DS_Store'])
+    deployment_data += generate_deployment_data("model", models_path, exclude_files=['.DS_Store'])
 
     print("Updating app.sml with deployment files...")
-    update_deployment(app_sml_path, deployment_data.strip())
-
+    update_deployment(app_sml_path, deployment_data)
 
 if __name__ == "__main__":
     update()
+
